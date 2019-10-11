4c708cdba830d692f708d3753dc115473f332c48
Andy Nguyen <anguyen1119@gmail.com>
2019-10-10 20:43:42 -0400

    Add store picker component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert('hey');
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+import './css/style.css';
+
+render(<StorePicker />, document.querySelector('#main'));
