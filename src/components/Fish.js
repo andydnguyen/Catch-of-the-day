e9fc415b8ebae0507aeee892714001bd78cb7ce6
Andy Nguyen <anguyen1119@gmail.com>
2019-10-13 15:11:22 -0400

    Add order state

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,6 +4,7 @@ import { formatPrice } from '../helpers';
 class Fish extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === 'available';
     return (
       <li className='menu-fish'>
         <img src={image} alt={name}></img>
@@ -12,7 +13,12 @@ class Fish extends React.Component {
           <span className='price'>{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button
+          disabled={!isAvailable}
+          onClick={() => this.props.addToOrder(this.props.index)}
+        >
+          {isAvailable ? 'Add To Cart' : 'Sold Out!'}
+        </button>
       </li>
     );
   }
